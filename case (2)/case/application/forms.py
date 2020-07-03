fee0a12f90cc3678077ba3d02ce50ffa3acebd97
jlokhande46 <jlokhande46@gmail.com>
2020-07-03 09:19:16 +0000

    Remove the now ignored directory "some-directory"

--- case (2)/case/application/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Length, ValidationError
-
-class LoginForm(FlaskForm):
-	username=StringField('username',validators=[DataRequired(),Length(min=2, max=20)])
-	password=PasswordField('password',validators=[DataRequired()])
-	submit = SubmitField('Sign In')
